cb95a3ef0ba6a879595c1e27938a9eb4f8d4c92c
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2024-01-27 17:32:34 -0500

    updating delete route in breads controller

--- views/show.jsx
+++ views/show.jsx
@@ -8,9 +8,9 @@ function Show ({bread, index}) {
         <Default>
             <h2>Show Page</h2>
             <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
-                <input type='submit' value="DELETE"/>
-            </form>
+           <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value="DELETE"/>
+        </form>
             <h3>{bread.name}</h3>
             <p>
                 and it
